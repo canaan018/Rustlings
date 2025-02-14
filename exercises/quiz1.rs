6ca967e55d90ae2a2b1f3093c5f7c93b960e8f73
zhouxukang <13851931750@163.com>
2025-02-14 15:04:14 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -17,6 +17,13 @@
 
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples (quantity: i32) -> i32 {
+    let mut price = 2;
+    if quantity > 40 {
+        price = 1;
+    }
+    return price * quantity;
+}
 
 // Don't modify this function!
 #[test]
